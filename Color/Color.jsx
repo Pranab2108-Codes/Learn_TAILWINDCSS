8ec6bf34e0c1fa2bed910c8a810f68128ab2063c
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-06-07 23:29:29 +0530

    Color.tailwind

---
+++ Color/Color.jsx
@@ -0,0 +1,29 @@
+export default function Color() {
+
+  return (
+
+    <div className="flex justify-center items-center h-screen">
+
+      <div className="bg-purple-500 text-blue-800 w-50 h-40">
+
+        Hi there from the first div
+
+      </div>
+
+    </div>
+    
+  );
+
+};
+
+
+// import { StrictMode } from 'react'                                    /* It should be written inside of src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css';
+// import Color from '../Color/Color.jsx';
+
+// createRoot(document.getElementById('root')).render(
+  
+//     <Color />
+  
+// );
