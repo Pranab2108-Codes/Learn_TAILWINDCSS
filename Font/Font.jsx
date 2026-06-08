78cba826757a3fc91f31bf9aae8e67234657d3a2
Pranab2108-Codes <pranab2108.codes@gmail.com>
2026-06-08 23:00:49 +0530

    Font.tailwind

---
+++ Font/Font.jsx
@@ -0,0 +1,29 @@
+export default function Font() {
+
+  return (
+
+    <div className="flex justify-center items-center h-screen">
+
+      <div className="flex justify-center items-center bg-slate-500 text-green-400 text-xs w-40 h-40">      
+      
+        Hi there from the first div
+
+      </div>
+
+    </div>
+
+  );
+
+};
+
+
+// import { StrictMode } from 'react'                        /* It should be written inside of src/main.jsx. */
+// import { createRoot } from 'react-dom/client'
+// import './index.css';
+// import Font from '../Font/Font.jsx';
+
+// createRoot(document.getElementById('root')).render(
+  
+//     <Font />
+  
+// );
